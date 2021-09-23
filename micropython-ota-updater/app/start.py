6df616c74976f12fdd1681cc417ab5a60fc9b46e
domeac129 <91245942+domeac129@users.noreply.github.com>
2021-09-23 21:20:15 +0700

    Update start.py

--- micropython-ota-updater/app/start.py
+++ micropython-ota-updater/app/start.py
@@ -1,1 +1,13 @@
 print('Version 2 installed using USB') 
+from machine import Pin
+from time import sleep
+
+
+led = Pin(2,Pin.OUT)
+
+for x in range (10):
+    led.value(1)
+    sleep(0.1)
+    led.value(0)
+    sleep(0.1)
+    print("1")
